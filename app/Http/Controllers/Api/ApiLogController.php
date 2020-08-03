e0ef84cf9c45e5da8975792370b1f9f12f4fe947
wanx <wanx@app.com>
2020-08-03 10:57:05 +0800

    添加按钮埋点

--- app/Http/Controllers/Api/ApiLogController.php
+++ app/Http/Controllers/Api/ApiLogController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ApiLogRequest;
 use App\Models\Api\User;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
 class ApiLogController extends Controller
@@ -18,12 +19,14 @@ class ApiLogController extends Controller
     $input = $request->input('stack', []);
     $userData = User::getUserData();
     $stacks = collect($input)->map(function ($item) use ($userData, $request) {
+      $input = Arr::get($item, 'input');
       return [
         'user_id' => $userData->id,
         'nickname' => $userData->nickname,
         'method' => $item['method'],
         'path' => $item['path'],
         'ip' => $request->getClientIp(),
+        'input' => $input ? json_encode($input) : null,
         'status' => 'success',
         'code' => 200,
         'desc' => $item['desc'],
