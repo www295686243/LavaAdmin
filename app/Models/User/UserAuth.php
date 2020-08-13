f333161f6c34f7ae0a899222496478d8acbc6fda
wanx <wanx@app.com>
2020-08-13 11:39:51 +0800

    微信登陆调试

--- app/Models/User/UserAuth.php
+++ app/Models/User/UserAuth.php
@@ -86,8 +86,10 @@ class UserAuth extends Base
 
     $plainTextToken = $userData->createToken('token')->plainTextToken;
     [$id, $token] = explode('|', $plainTextToken, 2);
+    \Log::info($token);
     $userData->api_token = $token;
     $userData->save();
+    \Log::info($userData);
     return $userData;
   }
 
